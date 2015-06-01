2871c182200b64c6350eb95a5005924877660f8f
Zhangfengjuan <wwzfj13ww@163.com>
2015-06-01 17:19:34 +0800

    commit init

---
+++ HelloWorld/HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by imagebook on 15-6-1.
+//  Copyright (c) 2015年 ZhangFengJuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by imagebook on 15-6-1.
+//  Copyright (c) 2015年 ZhangFengJuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
